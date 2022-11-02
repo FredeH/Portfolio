b810ae084c3ae5f17baf6a6cbc5d0d2ebd652ab3
FredeH <frede.hansen09@gmail.com>
2022-11-02 03:29:27 +0100

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,19 +6,19 @@ const Navbar = () => {
         <div className="text-white h-[80px] flex justify-between bg-[#0a0a0a] drop-shadow-md">
             <h1 className="w-full text-3xl font-bold text-[#d7dbe0] py-5 pl-5 border-l">Frederik Hansen</h1>
             <ul className="uppercase flex my-2 mx-2 rounded-md font-bold">
-                <a href="/">
+                <a href="#/">
                     <li className="p-4">Home</li>
                 </a>
-                <a href="/Portfolio">
+                <a href="#/Portfolio">
                     <li className="p-4">Portfolio</li>
                 </a>
-                <a href="/Projects">
+                <a href="#/Projects">
                     <li className="p-4">Projects</li>
                 </a>
-                <a href="/CV">
+                <a href="#/CV">
                     <li className="p-4">CV</li>
                 </a>
-                <a href="/Contact">
+                <a href="#npm/Contact">
                     <li className="p-4">Contact</li>
                 </a>
             </ul>
