63352a9500b472ff66af49b8cc481dc74c89bca9
FredeH <frede.hansen09@gmail.com>
2022-11-02 02:00:45 +0100

    Update

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Hero = () => {
+    return (
+        <div className="text-white">
+            <div className="max-w-[800px] w-full h-[500px] mx-auto text-center flex flex-col justify-center">
+                <p className="font-bold p-2 sm:text-6xl">Frederik Hansen</p>
+                <h1 className="sm:text-3xl">Developer and designer</h1>
+                <h1 className="sm:text-xl py-4">This page will show you some of the things that I've made</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { MdOutlineChevronRight, MdOutlineMenu } from 'react-icons/md'
+
+const Navbar = () => {
+    return (
+        <div className="text-white h-[80px] flex justify-between bg-[#0a0a0a] drop-shadow-md">
+            <h1 className="w-full text-3xl font-bold text-[#d7dbe0] py-5 pl-5 border-l">Frederik Hansen</h1>
+            <ul className="uppercase flex my-2 mx-2 rounded-md font-bold">
+                <a href="/">
+                    <li className="p-4">Home</li>
+                </a>
+                <a href="/Portfolio">
+                    <li className="p-4">Portfolio</li>
+                </a>
+                <a href="/Projects">
+                    <li className="p-4">Projects</li>
+                </a>
+                <a href="/CV">
+                    <li className="p-4">CV</li>
+                </a>
+                <a href="/Contact">
+                    <li className="p-4">Contact</li>
+                </a>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
